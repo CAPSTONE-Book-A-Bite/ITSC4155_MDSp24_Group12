71cd286c5042880e26beb63fe45d40a722e4a120
DESKTOP-72DJFR\Abrar <amian2@uncc.edu>
2024-04-08 18:13:15 -0400

    login changes and new customer page

---
+++ frontend/components/login.js
@@ -0,0 +1,23 @@
+document.getElementById("login-form").addEventListener("submit", async (event) => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const loginData = { email, password };
+    try {
+        const response = await fetch("http://localhost:3001/api/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginData),
+        });
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error(data.message);
+        }
+        localStorage.setItem("userId", data.userId);
+        // Redirect to home page
+        window.location.href = "/frontend/customer.html";
+    } catch (error) {
+        alert(error.message);
+    }
+});
